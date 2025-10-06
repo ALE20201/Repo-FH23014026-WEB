3c4bcdf5ccff5b03ffe07ccd04bf4dc5d171d5e8
Alex M <145505088+ALE20201@users.noreply.github.com>
2025-10-06 11:47:12 -0600

    idk

--- PP1 - Alex Monge Arias/PP1/obj/Debug/net8.0/PP1.AssemblyInfo.cs
+++ PP1 - Alex Monge Arias/PP1/obj/Debug/net8.0/PP1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f6b862370185dcc35c9808ad0b97e2606b87f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
