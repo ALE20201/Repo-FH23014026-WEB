2eecbbeebf4a7bdc512b75cde60d1acfbae91a83
Alex M <145505088+ALE20201@users.noreply.github.com>
2025-11-10 09:28:36 -0600

    no se que es esto
    chache de la pp1 nada de importancia

--- PP1 - Alex Monge Arias/PP1/obj/Debug/net8.0/PP1.AssemblyInfo.cs
+++ PP1 - Alex Monge Arias/PP1/obj/Debug/net8.0/PP1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143fd68e9d667f631f4ce930d63275c9eb495d66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a63638242097d573d5d307be36f5d51a1bbeb4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
